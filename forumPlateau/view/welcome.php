a3e1c6c26ef3c8224ea68691afb6c2d3326f8be7
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-22 14:37:57 +0200

    welcome.php: creating Welcome view with input

---
+++ forumPlateau/view/welcome.php
@@ -0,0 +1,14 @@
+<style>
+
+</style>
+
+<main>
+    <h1>Bienvenue</h1>
+    <form action="index.php?ctrl=security&action=login" method="post" id="login" >
+        <input type="button" value="Login" id="login">
+    </form>
+
+    <form action="index.php?ctrl=security&action=signup" method="post" id="sign up">
+        <input type="button" value="Sign up" id="sign up">
+    </form>
+</main>
