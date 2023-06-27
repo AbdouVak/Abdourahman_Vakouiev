2f11a8a4beff10f98a2c55b586047712c01167da
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-27 10:56:48 +0200

    Post: Create a entitie for post

---
+++ forumPlateau/model/entities/Post.php
@@ -0,0 +1,118 @@
+<?php
+    namespace Model\Entities;
+
+    use App\Entity;
+
+    final class Post extends Entity{
+
+        private $id;
+        private $texte;
+        private $creationdate;
+        private $topic;
+        private $user;
+
+        public function __construct($data){         
+            $this->hydrate($data);        
+        }
+ 
+        /**
+         * Get the value of id
+         */ 
+        public function getId()
+        {
+                return $this->id;
+        }
+
+        /**
+         * Set the value of id
+         *
+         * @return  self
+         */ 
+        public function setId($id)
+        {
+                $this->id = $id;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of texte
+         */ 
+        public function getTexte()
+        {
+                return $this->texte;
+        }
+
+        /**
+         * Set the value of texte
+         *
+         * @return  self
+         */ 
+        public function setTexte($texte)
+        {
+                $this->texte = $texte;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of creationdate
+         */ 
+        public function getCreationdate()
+        {
+                return $this->creationdate;
+        }
+
+        /**
+         * Set the value of creationdate
+         *
+         * @return  self
+         */ 
+        public function setCreationdate($creationdate)
+        {
+                $this->creationdate = $creationdate;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of topic
+         */ 
+        public function getTopic()
+        {
+                return $this->topic;
+        }
+
+        /**
+         * Set the value of topic
+         *
+         * @return  self
+         */ 
+        public function setTopic($topic)
+        {
+                $this->topic = $topic;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of user
+         */ 
+        public function getUser()
+        {
+                return $this->user;
+        }
+
+        /**
+         * Set the value of user
+         *
+         * @return  self
+         */ 
+        public function setUser($user)
+        {
+                $this->user = $user;
+
+                return $this;
+        }
+
+    }
