52a5e7586683598acb127f96ed8cec83597afb45
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-27 09:33:01 +0200

    listeTopics: add the code from listCategories to listTopics

--- forumPlateau/view/forum/listTopics.php
+++ forumPlateau/view/forum/listTopics.php
@@ -1,16 +1,26 @@
 <?php
 
 $topics = $result["data"]['topics'];
-    
+$categories = $result["data"]["categories"];
 ?>
 
 <h1>liste topics</h1>
 
+<select name="categorie" id='cateorie'>
+    <option value="">--Catégorie--</option><?php
+    foreach($categories as $categorie){?>
+
+            <option value="<?= $categorie->getCategorie();?>"> <?= $categorie->getCategorie();?></option>
+
+        <?php
+
+    }?>
+</select>
+
 <?php
 foreach($topics as $topic ){
-
     ?>
-    <p><?=$topic->getTitle()?></p>
+        <p><?=$topic->getTitle()?></p>
     <?php
 }
 
