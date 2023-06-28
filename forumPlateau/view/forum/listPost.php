fb8f899db45d913ae189ddbd3af386468a8e5f6c
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-28 16:53:37 +0200

    listPost: add a form to send post + display the title of the topic

--- forumPlateau/view/forum/listPost.php
+++ forumPlateau/view/forum/listPost.php
@@ -1,19 +1,38 @@
 <?php
 
 $posts = $result["data"]['posts'];
+$IDPost =  $result["data"]['IDPost'];
 
+$topicNames = $result["data"]["topicNames"];
+
+foreach($topicNames as $topicName){?>  
+    <h1> " <?= $topicName->getTitle()?> "</h1><?php
+}
 ?>
 
-<h1>Topic </h1>
-
+<p> --------------------------------------------------------------------------------------------------------------------- </p>
+   
 <?php
-foreach($posts as $post){
-    ?>  
+foreach($posts as $post){?>  
+
     <div>
-        <p> --------------------------------------------------------------------------------------------------------------------- </p>
         <p><?=$post->getUser()->getPseudo()?> - <?=$post->getCreationdate()?> </p>
         <p><?=$post->getTexte()?> </p>
         <p> --------------------------------------------------------------------------------------------------------------------- </p>
-    </div>
-    <?php
+   
+    </div><?php
 }
+
+?>
+
+<form action="index.php?ctrl=post&action=addPost&id=<?= $IDPost ?>" method='POST'id="submit" >
+
+
+    <label for="post">Post:</label><br>
+    <textarea name="post"rows="6" cols="50">
+    </textarea><br>
+        
+    <input type="submit" name="submit" value="submit">
+
+</form><?php
+?>
