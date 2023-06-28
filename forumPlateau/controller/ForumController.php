fbc478e94854c426cae39cc25579dc1f42a0b5ce
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-28 16:50:21 +0200

    ForumController: create a function will add a topic according to id category + send id,name category

--- forumPlateau/controller/ForumController.php
+++ forumPlateau/controller/ForumController.php
@@ -7,7 +7,7 @@
     use App\ControllerInterface;
     use Model\Managers\TopicManager;
     use Model\Managers\CategorieManager;
-
+    use Model\Managers\PostManager;
     class ForumController extends AbstractController implements ControllerInterface{
 
         public function index(){
@@ -19,13 +19,16 @@
                 "view" => VIEW_DIR."forum/listTopics.php",
                 "data" => [
                     "categories" => $CategorieManager->findAll(["categorie", "ASC"]),
-                    "topics" => $topicManager->findAll(["creationdate", "DESC"])
+                    "topics" => null,
+                    "addTopic" => false,
+                    "IDCategorie" => null,
+                    "categoryNames" => null
                 ]
             ];
             
         }
 
-        public function listTopicsByCategorie($id) {
+        public function listTopicsByCategorie($id){
             $CategorieManager = new CategorieManager();
             $topicManager = new TopicManager();
             
@@ -33,9 +36,46 @@
                 "view" => VIEW_DIR."forum/listTopics.php",
                 "data" => [
                     "categories" => $CategorieManager->findAll(["categorie", "ASC"]),
-                    "topics" => $topicManager->findTopicByID($id)
+                    "topics" => $topicManager->findTopicByID($id),
+                    "addTopic" => true,
+                    "IDCategorie" => $id,
+                    "categoryNames" => $CategorieManager->categorieName($id)
                 ]
             ];
             
+        }
+
+        public function addTopic($id){
+            
+            $topicManager = new TopicManager();
+            $postManager = new PostManager();
+
+            if(isset($_POST['submit'])){
+
+                if(isset($_POST['post']) && (!empty($_POST['post']))){
+                    $post = filter_input(INPUT_POST,"post",FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                    $title = filter_input(INPUT_POST,"title",FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                    
+                    
+                    if($post && $title) {
+                        $idTopicPublish = $topicManager->add([
+                            "title" => $title,
+                            "creationdate" => date('d-m-y h:i:s'),
+                            "user_id" => 1,
+                            "closed" => "open",
+                            "categorie_id" => $id,
+                        ]);
+
+                        $postManager->add([
+                            "texte" => $post,
+                            "creationdate" => date('d-m-y h:i:s'),
+                            "topic_id" => $idTopicPublish,
+                            "user_id" => 1
+                        ]);
+                    }
+                    
+                    header("Location:index.php?ctrl=forum&action=listTopicsByCategorie&id=$id");
+                }
+            }
         }
     }
