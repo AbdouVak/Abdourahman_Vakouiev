629339807155f51817c74fcdeac9e044646896a8
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-28 16:50:59 +0200

    PostController: create a function will add a post according to id topic + send id,name category

--- forumPlateau/controller/PostController.php
+++ forumPlateau/controller/PostController.php
@@ -6,7 +6,7 @@
     use App\AbstractController;
     use App\ControllerInterface;
     use Model\Managers\PostManager;
-
+    use Model\Managers\TopicManager;
     class PostController extends AbstractController implements ControllerInterface{
 
         public function index(){
@@ -15,14 +15,38 @@
 
         public function listPostByTopic($id){
             $postManager = new PostManager();
+            $topicManager = new TopicManager();
 
             return [
                 "view" => VIEW_DIR."forum/listPost.php",
                 "data" => [
-                    "posts" => $postManager->findPostByTopic($id)
+                    "posts" => $postManager->findPostByTopic($id),
+                    "IDPost" => $id,
+                    "topicNames" => $topicManager->topicNames($id)
                 ]
             ];
         }
         
+        public function addPost($id){
+            
+            $postManager = new PostManager();
+            if(isset($_POST['submit'])){
 
+                if(isset($_POST['post']) && (!empty($_POST['post']))){
+                    $post = filter_input(INPUT_POST,"post",FILTER_SANITIZE_FULL_SPECIAL_CHARS);    
+                    
+                    if($post ) {
+
+                        $postManager->add([
+                            "texte" => $post,
+                            "creationdate" => date('d-m-y h:i:s'),
+                            "topic_id" => $id,
+                            "user_id" => 1,
+                        ]);
+                    }
+                    
+                    header("Location:index.php?ctrl=post&action=listPostByTopic&id=$id");
+                }
+            }
+        }
     }
