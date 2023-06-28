0b99d28f54ab100d8f211e268a08b2d753c22a84
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-28 11:19:40 +0200

    listTopic: add a form to create/send a post

--- forumPlateau/view/forum/listTopics.php
+++ forumPlateau/view/forum/listTopics.php
@@ -3,23 +3,53 @@
 $categories = $result["data"]["categories"];
 $topics = $result["data"]['topics'];
 
+
 ?>
 
 <h1>liste topics</h1>
 
 
 <?php
-foreach($categories as $categorie){?>  
-    <a href='index.php?ctrl=forum&action=listTopics&id=<?=$categorie->getID()?>'><?=$categorie->getCategorie()?></a><?php
-}
 
 
-foreach($topics as $topic){
-
-    if($topic->getTitle() != null){?>
-        <div>
-            <a href="index.php?ctrl=post&action=topics"><p><?=$topic->getTitle()?> - <?=$topic->getCreationdate()?></p></a>
-        </div><?php
+    foreach($categories as $categorie){?>  
+        <a href='index.php?ctrl=forum&action=listTopicsByCategorie&id=<?=$categorie->getID()?>'><?=$categorie->getCategorie()?></a><?php
     }
-}
+
+
+
+
+if(isset($topics)){
+    foreach($topics as $topic){
+
+        if($topic->getTitle() != null){?>
+            <div>
+                <a href="index.php?ctrl=post&action=listPostByTopic&id=<?=$topic->getID()?>"><p><?=$topic->getTitle()?> - <?=$topic->getCreationdate()?></p></a>
+            </div><?php
+        }
+        
+    }
+}else{?>
+
+    <p>Pas de topic pour cette categorie</p><?php
+
+}?>
+<p>----------------------------------------------------------</p>
+
+<form>
+
+    <label for="name">Titre:</label>
+    <input type="text" id="name" name="name" required
+        minlength="4" maxlength="8" size="10"> <?= date('d-m-y h:i:s');?><br></br>
+
+
+    <label for="post">Post:</label><br>
+    <textarea id="post" name="post"rows="6" cols="50">
+    </textarea><br>
+
+    
+    <input type="submit">
+
+    
+</form>
   
