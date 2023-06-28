5907866df6acdaa86e7777f242503a42a4984e39
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-28 11:16:22 +0200

    ForumController: create a function to list topics by categorie

--- forumPlateau/controller/ForumController.php
+++ forumPlateau/controller/ForumController.php
@@ -7,13 +7,14 @@
     use App\ControllerInterface;
     use Model\Managers\TopicManager;
     use Model\Managers\CategorieManager;
+
     class ForumController extends AbstractController implements ControllerInterface{
 
         public function index(){
 
             $CategorieManager = new CategorieManager();
             $topicManager = new TopicManager();
-
+            
             return [
                 "view" => VIEW_DIR."forum/listTopics.php",
                 "data" => [
@@ -21,11 +22,20 @@
                     "topics" => $topicManager->findAll(["creationdate", "DESC"])
                 ]
             ];
-
             
-        
         }
 
-        
-
+        public function listTopicsByCategorie($id) {
+            $CategorieManager = new CategorieManager();
+            $topicManager = new TopicManager();
+            
+            return [
+                "view" => VIEW_DIR."forum/listTopics.php",
+                "data" => [
+                    "categories" => $CategorieManager->findAll(["categorie", "ASC"]),
+                    "topics" => $topicManager->findTopicByID($id)
+                ]
+            ];
+            
+        }
     }
