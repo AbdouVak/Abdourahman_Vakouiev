f94b6956d2e48f9e8d8cfb87fbfcba53bd12f8c1
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-29 16:59:55 +0200

    Create register to have a view of the register page

---
+++ forumPlateau/view/security/register.php
@@ -0,0 +1,17 @@
+<form action="index.php?ctrl=security&action=register" method='POST'id="submitRegister" >
+
+    <label for="pseudo">Pseudo:</label>
+    <input type="text" name="pseudo"size="10"><br>
+
+    <label for="email">Email:</label>
+    <input type="text" name="email"size="10"><br>
+
+    <label for="password">Password:</label>
+    <input type="text" name="password"size="10"><br>
+
+    <label for="passwordVerif">Password verification:</label>
+    <input type="text" name="passwordVerif"size="10"><br>
+
+    <input type="submit" name="submitRegister" value="Register">
+
+</form>
