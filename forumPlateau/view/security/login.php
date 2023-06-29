fe4799521932c4ebe64bb1ef315205e9017a68f4
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-29 17:00:07 +0200

    Create login to have a view of the login page

---
+++ forumPlateau/view/security/login.php
@@ -0,0 +1,11 @@
+<form action="index.php?ctrl=security&action=login" method='POST'id="submitLogin" >
+
+    <label for="email">Email:</label>
+    <input type="text" name="email"size="10"><br>
+
+    <label for="password">Password:</label>
+    <input type="text" name="password"size="10"><br>
+
+    <input type="submit" name="submitLogin" value="Login">
+
+</form>
